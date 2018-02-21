73b3064d5555dfccca85df34a8a4216ffa535d9d
sanjeevnvp <sanjeev.p@egovernments.org>
2018-02-21 18:18:40 +0530

    [PHOENIX-7695] : Rule 30 changes

--- egov/egov-edcr/src/main/java/org/egov/edcr/rule/Rule30.java
+++ egov/egov-edcr/src/main/java/org/egov/edcr/rule/Rule30.java
@@ -1,26 +1,20 @@
 package org.egov.edcr.rule;
 
 import org.egov.edcr.entity.PlanDetail;
-import org.egov.edcr.entity.Result;
-import org.egov.edcr.entity.utility.RuleReportOutput;
 import org.egov.edcr.utility.DcrConstants;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 
 @Service
 public class Rule30 extends GeneralRule {
 
-    public static final String OCCUPANCY_A1 = "Occupancy A1";
-
     @Override
     public PlanDetail validate(PlanDetail planDetail) {
-        HashMap<String, String> errors = new HashMap<String, String>();
+        HashMap<String, String> errors = new HashMap<>();
         System.out.println("validate 30");
         if (planDetail != null && planDetail.getPlanInformation() != null
                 && StringUtils.isEmpty(planDetail.getPlanInformation().getOccupancy())) {
@@ -30,40 +24,6 @@ public class Rule30 extends GeneralRule {
             planDetail.addErrors(errors);
         }
         return planDetail;
-    }
-
-    @Override
-    public PlanDetail process(PlanDetail planDetail) {
-        List<RuleReportOutput> ruleReportOutputs = new ArrayList<>();
-
-        rule30(planDetail, ruleReportOutputs);
-
-        return planDetail;
-    }
-
-    private List<RuleReportOutput> rule30(PlanDetail planDetail, List<RuleReportOutput> ruleReportOutputs) {
-
-        RuleReportOutput ruleReportOutput;
-
-        if (planDetail != null && planDetail.getPlanInformation() != null
-                && !StringUtils.isEmpty(planDetail.getPlanInformation().getOccupancy())
-                && planDetail.getPlanInformation().getOccupancy().equalsIgnoreCase(OCCUPANCY_A1)) {
-            ruleReportOutput = new RuleReportOutput();
-            ruleReportOutput.setRuleKey(DcrConstants.RULE30);
-            ruleReportOutput.setFieldVerified(DcrConstants.OCCUPANCY);
-            ruleReportOutput.setActualResult(Result.Accepted.toString());
-            ruleReportOutput.setExpectedResult(DcrConstants.EXPECTEDRESULT);
-            ruleReportOutputs.add(ruleReportOutput);
-        } else {
-            ruleReportOutput = new RuleReportOutput();
-            ruleReportOutput.setRuleKey(DcrConstants.RULE30);
-            ruleReportOutput.setFieldVerified(DcrConstants.OCCUPANCY);
-            ruleReportOutput.setActualResult(Result.Accepted.toString());
-            ruleReportOutput.setExpectedResult(DcrConstants.EXPECTEDRESULT);
-            ruleReportOutputs.add(ruleReportOutput);
-        }
-        return ruleReportOutputs;
-
     }
 
 }
