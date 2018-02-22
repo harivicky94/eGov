a2ae110c341e622f7c8508a39b7b6457113a0083
Pradeep <pradeep@egovernments.org>
2018-02-22 20:30:45 +0530

    [PHOENIX-7695] Rules are defined based on area

--- egov/egov-edcr/src/main/java/org/egov/edcr/service/PlanRuleService.java
+++ egov/egov-edcr/src/main/java/org/egov/edcr/service/PlanRuleService.java
@@ -1,5 +1,6 @@
 package org.egov.edcr.service;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,7 +32,41 @@ public class PlanRuleService {
     public List<PlanRule> findRulesByPlanDetail(PlanDetail planDetail) {
         // TODO Auto-generated method stub
         PlanRule planRule = new PlanRule();
-        planRule.setRules("23,26,60,61,62");
+        StringBuffer planRules= new StringBuffer();
+        planRules.append("23,30"); //Default rule to be add.// overheadline and other
+        BigDecimal plotArea;
+        if(planDetail!=null && planDetail.getPlanInformation()!=null && planDetail.getPlanInformation().getPlotArea()!=null)
+        {
+            plotArea = planDetail.getPlanInformation().getPlotArea();
+
+            if (plotArea.compareTo(BigDecimal.valueOf(125)) <= 0) // Plot area less than 125
+            {
+                planRules.append(",26");// construction abuting to public road
+                planRules.append(",61");
+                planRules.append(",62");
+                planRules.append(",63");
+                planRules.append(",64");
+
+            } else {
+                planRules.append(",24"); // interior or exterior
+                planRules.append(",25");// distance from road
+                planRules.append(",31");// coverage and FAR
+
+                if (planDetail.getBuilding().getMaxFloor() != null
+                        && planDetail.getBuilding().getMaxFloor().compareTo(BigDecimal.valueOf(3)) > 0) {
+                    // if floor greater than 3 floor validate.
+                    planRules.append(",32"); // Height of building
+                } else {
+                    // Append rule from 35 to 50.
+                }
+
+                planRules.append(",33");// access to plot
+                planRules.append(",34");// Parking
+
+            }
+
+        }
+        planRule.setRules(planRules.toString());
         List<PlanRule> rules = new ArrayList<>();
         rules.add(planRule);
         return rules;
