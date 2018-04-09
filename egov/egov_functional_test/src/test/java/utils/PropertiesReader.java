322f860f8a0743c3bf5f96e120b8fd918afb6ef9
Akhilagd <akhila.gd@egovernaments.org>
2018-04-09 17:43:41 +0530

    [PHOENIX-7884] Fetch url credentials from gradle properties

--- egov/egov_functional_test/src/test/java/utils/PropertiesReader.java
+++ egov/egov_functional_test/src/test/java/utils/PropertiesReader.java
@@ -1,47 +1,51 @@
 package utils;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertiesReader {
     private Properties prop = new Properties();
+    InputStream input = null;
+    String env = System.getProperty("env");
 
     public PropertiesReader() {
         try {
-            String env = System.getProperty("env");
+
             System.out.println("ENV: " + env);
+//
+//            if (null == env) env = "local";
 
-            if (null == env) env = "local";
+//            String propertiesFilePath = "/gradle.properties";
+//            InputStream inputStream;
+//            inputStream = getInputStream(propertiesFilePath);
+//            prop.load(inputStream);
+            input = new FileInputStream("gradle.properties");
 
-            String propertiesFilePath = env + ".properties";
-            InputStream inputStream;
-            inputStream = getInputStream(propertiesFilePath);
-            prop.load(inputStream);
+            // load a properties file
+            prop.load(input);
 
+            // get the property value and print it out
+//            System.out.println(prop.getProperty("dbuser"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private InputStream getInputStream(String propertiesFilePath) {
-        return this.getClass().getClassLoader().getResourceAsStream(propertiesFilePath);
-    }
-
+//    private InputStream getInputStream(String propertiesFilePath) {
+//        return this.getClass().getClassLoader().getResourceAsStream(propertiesFilePath);
+//    }
 
     public String getUrl() {
-        return prop.getProperty("url");
+        return prop.getProperty(env+".url");
     }
 
-    public String getDburl(){return prop.getProperty("dburl");}
+    public String getDburl(){return prop.getProperty(env+".dburl");}
 
-    public String getDbuser(){return prop.getProperty("dbuser");}
+    public String getDbuser(){return prop.getProperty(env+".dbuser");}
 
-    public String getDbpassword(){return prop.getProperty("dbpassword");}
-
-    public String getWaitTime() {
-        return prop.getProperty("waitTime");
-    }
+    public String getDbpassword(){return prop.getProperty(env+".dbpassword");}
 
 }
 
